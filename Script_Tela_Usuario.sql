9caa1614e61e1900f9a96e58c62850f5c63a9586
Pedro Alves <phda.pedro1606@gmail.com>
2022-03-18 20:24:05 -0300

    Fix Banco de Dados

--- Script_Tela_Usuario.sql
+++ Script_Tela_Usuario.sql
@@ -14,15 +14,29 @@ Create Table Usuario(
 	saida varchar(5) not null);
 	
 	
+
+Create Table Perguntas(
+	id_pergunta int(2),
+	PRIMARY KEY (id_pergunta),
+	Pergunta_Anamnese Varchar(100)
+);
+
 Create Table Paciente(
-	id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	id_paciente INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	Nome Varchar(50) not null,
 	Idade INT(3) NOT NULL,
+	Nascimento int(10) NOT NULL,
 	Escolaridade Varchar(20) NOT NULL,
 	Estado_Civil Varchar(20) NOT NULL,
-	Filhos INT(1) NOT NULL,
-	
-	
-)
+	Filhos INT(1) NOT NULL
+);
 
+Create Table Anamnese(
+	id_pergunta int(2),
+	id_paciente int(3) UNSIGNED,
+	resposta Varchar(400) not null,
+	FOREIGN KEY (id_pergunta) REFERENCES Perguntas(id_pergunta),
+	FOREIGN KEY (id_paciente) REFERENCES Paciente(id_paciente),
+	Primary KEY (id_pergunta,id_paciente)
+);
 	
